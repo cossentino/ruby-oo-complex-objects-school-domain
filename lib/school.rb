f3545a6c6753f424d7cdb535fd1969b4686c641c
Ian Cossentino <icossentino@gmail.com>
2020-11-12 11:04:39 -0500

    commit tree 7fd42e4469700e399e137556f3dbfcc62c787139

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
-# code here!
+
+
+class School
+
+    attr_accessor :roster, :name
+
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+
+    def add_student(student, grade)
+        if !@roster[grade]
+            @roster[grade] = [student]
+        else
+            @roster[grade] << student
+        end
+    end
+
+    def grade(grade_arg)
+        @roster[grade_arg]
+    end
+
+    def sort
+        @roster.each do |grade, students|
+            students.sort!
+        end
+    end
+        
+
+
+end
+    
